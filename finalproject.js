34ae831cfb0d7a573451aa9206b1abc54b379236
iths-jennifer-bunner <54809580+iths-jennifer-bunner@users.noreply.github.com>
2020-08-13 11:29:19 +0200

    added git ignore

--- finalproject.js
+++ finalproject.js
@@ -13,3 +13,18 @@ const svg = d3.select('.container')
 const chart = svg.append("g")
             .attr("class", "chart")
             .attr("transform", `translate(${margin},${margin})`)              
+
+const axesGroup = chart.append("g")
+                    .attr("class", "axes")
+
+const bottomAxisGroup = axesGroup.append("g")
+                    .attr("class", "bottom-axis")
+                    .attr("transform", `translate(0,${height-margin*2})`)
+
+const leftAxisGroup = axesGroup.append("g")
+            .attr("class", "left-axis")   
+
+const barsGroup = chart.append('g')
+            .attr('class', 'bars')            
+
+            
